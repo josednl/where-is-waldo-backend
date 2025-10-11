b4e2fad7e2144d8d82e186ea3d41b098ce86c038
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2025-10-11 10:52:34 -0500

    chore: add env variable PORT

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import leaderboardsRoutes from './routes/leaderboards.routes';
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
